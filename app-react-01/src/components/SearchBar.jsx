2c14aa22d418362a9f9354be7c41c8841a64f9ab
Lupercio Neto <lupercionetof@gmail.com>
2026-02-01 13:06:29 -0300

    feat: Aplicação do comportamento de SearchBar

--- app-react-01/src/components/SearchBar.jsx
+++ app-react-01/src/components/SearchBar.jsx
@@ -1,10 +1,23 @@
+import { UseState } from "react";
 import { BsSearch } from "react-icons/bs";
 import "./SearchBar.css";
 
-const SearchBar = () => {
+const SearchBar = ({ handleSearch }) => {
+    const [query, setQuery] = useState("");
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        handleSearch(query);
+    };
+
     return (
-        <form className="search-container">
-            <input type="text" placeholder="Pesquise por um modelo de carro..." />
+        <form className="search-container" onSubmit={handleSubmit}>
+            <input 
+                type="text" 
+                placeholder="Pesquise por um modelo de carro..." 
+                value={query || ""}
+                onChange={(e) => setQuery(e.target.value)}
+            />
             <button type="submit">
                 <BsSearch />
             </button>
